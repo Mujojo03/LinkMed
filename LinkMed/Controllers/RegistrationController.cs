f450c664120ce46acc9197ccf31c5268cf39c425
jane rose <rosejaneforyou@gmail.com>
2024-09-11 19:35:45 +0300

    Added the User Registration

---
+++ LinkMed/Controllers/RegistrationController.cs
@@ -0,0 +1,40 @@
+﻿using LinkMed.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+//using LinkMed.Models;
+
+namespace LinkMed.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {
+        private readonly IUserRegistrationService _userRegistrationService;
+        public RegistrationController(IUserRegistrationService userRegistrationService)
+        {
+            _userRegistrationService = userRegistrationService;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] IUserRegistrationService model)
+        {
+            if(model.Password != model.ConfirmPassword)
+            {
+                return BadRequest(new { message = "Password do notmatch." });
+            }
+
+            if(_userRegistrationService.UserExists(model.Username, model.Email))
+            {
+                return Conflict(new { message = "Username or Email already exists." });
+            }
+
+            var userCreated = _userRegistrationService.CreateUser(model.Email, model.Username, model.Password);
+            if (userCreated)
+            {
+                return Ok(new { message = "User Registered Successfully." });
+            }
+            return BadRequest(new { message = "Failed to Create User." });
+        }
+    }
+}
---
+++ LinkMed/IServices/IUserRegistrationService.cs
@@ -0,0 +1,8 @@
+﻿namespace LinkMed.IServices
+{
+    public interface IUserRegistrationService
+    {
+        bool UserExists(string username, string email);
+        bool CreateUser(string username, string email, string password);
+    }
+}

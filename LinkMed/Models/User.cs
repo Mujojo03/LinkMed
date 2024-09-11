119c4ca0d044b07090bff03dca93cd983acf0880
jane rose <rosejaneforyou@gmail.com>
2024-09-11 16:00:24 +0300

    Added code to dev branch

---
+++ LinkMed/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LinkMed.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(9)]
+        public string Password { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        //public bool PasswordUpdated { get; set; }
+    }
+}
